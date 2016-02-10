3a853e17f606cc6e3c968ed939968b90166ea5a3
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-02-09 22:36:15 -0500

    add htmlparser

---
+++ blog.js
@@ -0,0 +1,47 @@
+(function () {
+
+    function loadBlogData() {
+
+        return new Promise(function (resolve, reject) {
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', "blog.xml", true);
+
+            xhr.onload = function (e) {
+
+                if (this.status < 400) {
+
+                    var handler = new Tautologistics.NodeHtmlParser.DefaultHandler(function (error, dom) {
+                        if (error) {
+                            reject();
+                        } else {
+                            resolve();
+                        }
+                    });
+
+                    var parser = new Tautologistics.NodeHtmlParser.Parser(handler);
+                    parser.parseComplete(this.response);
+                    alert(JSON.stringify(handler.dom, null, 2));
+
+                } else {
+                    reject();
+                }
+            };
+
+            xhr.onerror = reject;
+
+            xhr.send();
+        });
+    }
+
+    function renderBlog() {
+
+        loadBlogData().then(function () {
+
+
+        });
+    }
+
+    renderBlog();
+
+})();
