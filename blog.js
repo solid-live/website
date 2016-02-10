4cf1292072d8e0022ca9572d980783a71400f2f7
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-02-09 22:56:53 -0500

    update blog

--- blog.js
+++ blog.js
@@ -24,6 +24,36 @@
         });
     }
 
+    function getBlogItemHtml(item) {
+
+        var title = item.querySelector('title').innerHTML;
+        var link = item.querySelector('link').innerHTML;
+        var pubDate = item.querySelector('pubDate').innerHTML;
+
+        var imageElement = item.querySelector('image url').innerHTML;
+        var imageUrl = imageElement ? imageElement.innerHTML : null;
+
+        var html = '';
+
+        html += '<a class="card" href="' + link + '">';
+
+        html += '<div class="cardContent">';
+
+        html += '<div class="cardTitle">';
+        html += title;
+        html += '</div>';
+
+        html += '<div class="cardDate">';
+        html += pubDate;
+        html += '</div>';
+
+        html += '</div>';
+
+        html += '</a>';
+
+        return html;
+    }
+
     function renderBlog() {
 
         loadBlogData().then(function (xml) {
@@ -32,8 +62,11 @@
             div.innerHTML = xml;
 
             var items = div.querySelectorAll('item');
-            alert(items.length);
 
+            var html = '';
+            for (var i = 0, length = items.length; i < length; i++) {
+                html += getBlogItemHtml(item);
+            }
         });
     }
 
